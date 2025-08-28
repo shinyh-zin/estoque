19225465915cc0012c687c79ed76b169d416cce0
Jean Leite <jeanventuraleite@gmail.com>
2025-08-28 11:28:24 -0400

    Fim do primeiro Dia 1
    Fim do dia foi assim que o codigo ficou

--- produtos/models.py
+++ produtos/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Produtos(models.Model):
     nome = models.CharField(max_length=100)
-    preço = models.DecimalField(max_digits=10, decimal_places=2)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
     quantidade = models.IntegerField()
     descricao = models.TextField(blank=True)
-    
+    def __str__(self):
+        return self.nome
+ 
