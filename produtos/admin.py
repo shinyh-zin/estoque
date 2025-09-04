10f65da425fe9f1b692c8e8808887327508cf2ef
Jean Leite <jeanventuraleite@gmail.com>
2025-09-04 11:52:21 -0400

    dia 2

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produtos
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'quantidade',)
+    list_filter = ('preco',)
+    search_fields = ('nome',)
+admin.site.register(Produtos, ProdutoAdmin)
--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Produtos
+def lista_produtos(request):
+    Produtos = Produtos.objects.all()
+    return render(request, 'produtos/lista.html', {'produtos':Produtos})
